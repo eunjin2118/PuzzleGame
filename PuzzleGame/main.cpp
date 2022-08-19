8f4e4379e71f1fc4b01b7628505f11d74b2d68f0
은진 <80873499+eunjin2118@users.noreply.github.com>
2022-08-19 09:04:39 +0900

    이미지 넣기

--- PuzzleGame/main.cpp
+++ PuzzleGame/main.cpp
@@ -4,7 +4,12 @@ using namespace sf;
 
 int main(void)
 {
-	RenderWindow app(VideoMode(256, 256), "PuzzleGame");
+	RenderWindow app(VideoMode(256, 256), "PuzzleGame"); // 그림사이즈는 2의 배수로 하는것이 좋다!
+	app.setFramerateLimit(60); //1초동안 60개의애니메이션을 실행? 적용? 할 수 있음
 
+	Texture t;
+	t.loadFromFile("imegas/15.png");
+
+	int w = 64; // 퍼즐 하나의 폭
 	return 0;
 }
